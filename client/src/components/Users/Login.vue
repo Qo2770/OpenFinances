7b3c3d8dd8d6b1a0ed12599b435a3ffd5b305bc9
Quentin Oschatz <quentin@cliffhouse.de>
2018-03-28 11:48:22 +0200

    feat: Implement form validation
    Implement form validation using vuelidate

--- client/src/components/Users/Login.vue
+++ client/src/components/Users/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="login">
-    <form class="md-layout" action="/auth" method="post">
+    <form class="md-layout" action="/auth" method="post" @submit.prevent="validateUser" novalidate>
       <md-card class="md-layout-item md-size-50 md-small-size-100">
         <md-card-header>
           <div class="md-title">Login</div>
@@ -9,18 +9,28 @@
         <md-card-content>
           <div class="md-layout md-gutter">
             <div class="md-layout-item md-small-size-100">
-              <md-field>
+              <md-field :class="getValidationClass('username')">
                 <label for="username">Username</label>
-                <md-input name="username" id="username" required />
+                <md-input name="username" id="username" autocomplete="username" v-model="form.username" :disabled="sending" />
+                <span class="md-error" v-if="!$v.form.username.required">Please enter your username</span>
               </md-field>
 
-              <md-field>
+              <md-field :class="getValidationClass('password')">
                 <label for="password">Password</label>
-                <md-input name="password" id="password" type="password" required />
+                <md-input name="password" id="password" type="password" v-model="form.password" :disabled="sending" />
+                <span class="md-error" v-if="!$v.form.password.required">Please enter you password</span>
               </md-field>
+
+              <div>
+                <md-checkbox v-model="form.rememberMe" :disabled="sending">Remember Me</md-checkbox>
+              </div>
+
+              <div class="md-error red right" style="color: red;" v-if="invalid"><small>Invalid username or password!</small></div>
+
+              <md-progress-bar md-mode="indeterminate" v-if="sending" />
 
               <md-card-actions>
-                <md-button type="submit" class="md-accent center">Login</md-button>
+                <md-button type="submit" class="md-accent center" :disabled="sending">Login</md-button>
               </md-card-actions>
 
             </div>
@@ -33,14 +43,68 @@
 
 <script>
 
+import { validationMixin } from 'vuelidate'
+import { required } from 'vuelidate/lib/validators'
+
 export default {
   name: 'login',
+  mixins: [validationMixin],
   components: {
 
   },
   data: () => ({
+    form: {
+      username: null,
+      password: null,
+      rememberMe: false
+    },
+    sending: false,
+    invalid: false
+  }),
+  validations: {
+    form: {
+      username: {
+        required
+      },
+      password: {
+        required
+      }
+    }
+  },
+  methods: {
+    getValidationClass (fieldName) {
+      const field = this.$v.form[fieldName]
 
-  })
+      if (field) {
+        return {
+          'md-invalid': field.$invalid && field.$dirty
+        }
+      }
+    },
+    clearForm () {
+      this.$v.$reset()
+      this.form.username = null
+      this.form.password = null
+      this.form.rememberMe = false
+    },
+    authUser () {
+      this.sending = true
+
+      // Instead of this timeout, here you can call your API
+      window.setTimeout(() => {
+        this.lastUser = `${this.form.firstName} ${this.form.lastName}`
+        this.sending = false
+        this.clearForm()
+      }, 1500)
+    },
+    validateUser () {
+      this.$v.$touch()
+
+      if (!this.$v.$invalid) {
+        this.authUser()
+      }
+    }
+  }
 }
 </script>
 
@@ -48,9 +112,5 @@ export default {
 #login {
 
 
-}
-
-.md-field::after {
-  display: none;
 }
 </style>
--- client/src/components/Users/Signup.vue
+++ client/src/components/Users/Signup.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="signup">
-    <form class="md-layout" action="/signup/adduser" method="post">
+    <form class="md-layout" action="/ads/adduser" method="post" @submit.prevent="validateUser" novalidate>
       <md-card class="md-layout-item md-size-50 md-small-size-100">
         <md-card-header>
           <div class="md-title">Sign Up</div>
@@ -10,32 +10,62 @@
           <div class="md-layout md-gutter">
             <div class="md-layout-item md-small-size-100">
 
-              <md-field>
+              <md-field :class="getValidationClass('email')">
                 <label for="email">Email</label>
-                <md-input name="email" id="email" type="email" required />
+                <md-input type="email" name="email" id="email" autocomplete="email" v-model="form.email" :disabled="sending" />
+                <span class="md-error" v-if="!$v.form.email.required">The email is required</span>
+                <span class="md-error" v-else-if="!$v.form.email.email">Invalid email</span>
               </md-field>
 
-              <md-field>
-                <label for="username">Username</label>
-                <md-input name="username" id="username" required />
-              </md-field>
 
-              <md-field>
-                <label for="password">Password</label>
-                <md-input name="password" id="password" type="password" required />
-              </md-field>
+              <div class="md-layout md-gutter">
+                <div class="md-layout-item md-small-size-100">
+                  <md-field :class="getValidationClass('firstName')">
+                    <label for="first-name">First Name</label>
+                    <md-input name="first-name" id="first-name" autocomplete="given-name" v-model="form.firstName" :disabled="sending" />
+                    <span class="md-error" v-if="!$v.form.firstName.required">The first name is required</span>
+                    <span class="md-error" v-else-if="!$v.form.firstName.minlength">Invalid first name</span>
+                  </md-field>
+                </div>
 
-              <md-field>
-                <label for="repassword">Re-Password</label>
-                <md-input name="repassword" id="repassword" type="password" required />
-              </md-field>
-
-              <div>
-                <md-checkbox v-model="tos">I Agree with the Terms of Service</md-checkbox>
+                <div class="md-layout-item md-small-size-100">
+                  <md-field :class="getValidationClass('lastName')">
+                    <label for="last-name">Last Name</label>
+                    <md-input name="last-name" id="last-name" autocomplete="family-name" v-model="form.lastName" :disabled="sending" />
+                    <span class="md-error" v-if="!$v.form.lastName.required">The last name is required</span>
+                    <span class="md-error" v-else-if="!$v.form.lastName.minlength">Invalid last name</span>
+                  </md-field>
+                </div>
               </div>
 
+              <md-field :class="getValidationClass('username')">
+                <label for="username">Username</label>
+                <md-input name="username" id="username" autocomplete="username" v-model="form.username" :disabled="sending" />
+                <span class="md-error" v-if="!$v.form.username.required">A username is required</span>
+                <span class="md-error" v-else-if="!$v.form.username.minlength">Invalid username</span>
+              </md-field>
+
+              <md-field :class="getValidationClass('password')">
+                <label for="password">Password</label>
+                <md-input name="password" id="password" type="password" v-model="form.password" :disabled="sending" />
+                <span class="md-error" v-if="!$v.form.password.required">A password is required</span>
+                <span class="md-error" v-if="!$v.form.password.minLength">Password is too short</span>
+              </md-field>
+
+              <md-field :class="getValidationClass('repeatPassword')">
+                <label for="repassword">Re-Password</label>
+                <md-input name="repassword" id="repassword" type="password" v-model="form.repeatPassword" required :disabled="sending" @input="$v.repeatPassword.$touch()" />
+                <span class="md-error" v-if="!$v.form.repeatPassword.sameAsPassword">Passwords must match</span>
+              </md-field>
+
+              <div :class="getValidationClass('tos')">
+                <md-checkbox v-model="form.tos" :disabled="sending">I Agree with the Terms of Service</md-checkbox>
+              </div>
+
+              <md-progress-bar md-mode="indeterminate" v-if="sending" />
+
               <md-card-actions>
-                <md-button type="submit" class="md-accent center">Sign Up</md-button>
+                <md-button type="submit" class="md-accent center" :disabled="sending">Sign Up</md-button>
               </md-card-actions>
 
             </div>
@@ -47,15 +77,100 @@
 </template>
 
 <script>
+import { validationMixin } from 'vuelidate'
+import {
+  required,
+  email,
+  minLength,
+  sameAs
+} from 'vuelidate/lib/validators'
 
 export default {
   name: 'signup',
+  mixins: [validationMixin],
   components: {
 
   },
   data: () => ({
-    tos: false
-  })
+    form: {
+      email: null,
+      username: null,
+      firstName: null,
+      lastName: null,
+      password: null,
+      repeatPassword: null,
+      tos: false
+    },
+    sending: false
+  }),
+  validations: {
+    form: {
+      username: {
+        required,
+        minLength: minLength(3)
+      },
+      firstName: {
+        required,
+        minLength: minLength(3)
+      },
+      lastName: {
+        required,
+        minLength: minLength(3)
+      },
+      email: {
+        required,
+        email
+      },
+      password: {
+        required,
+        minLength: minLength(6)
+      },
+      repeatPassword: {
+        sameAsPassword: sameAs('password')
+      },
+      tos: {
+        required
+      }
+    }
+  },
+  methods: {
+    getValidationClass (fieldName) {
+      const field = this.$v.form[fieldName]
+
+      if (field) {
+        return {
+          'md-invalid': field.$invalid && field.$dirty
+        }
+      }
+    },
+    clearForm () {
+      this.$v.$reset()
+      this.form.firstName = null
+      this.form.lastName = null
+      this.form.username = null
+      this.form.email = null
+      this.form.password = null
+      this.form.repeatPassword = null
+      this.form.tos = false
+    },
+    saveUser () {
+      this.sending = true
+
+      // Instead of this timeout, here you can call your API
+      window.setTimeout(() => {
+        this.lastUser = `${this.form.firstName} ${this.form.lastName}`
+        this.sending = false
+        this.clearForm()
+      }, 1500)
+    },
+    validateUser () {
+      this.$v.$touch()
+
+      if (!this.$v.$invalid) {
+        this.saveUser()
+      }
+    }
+  }
 }
 </script>
 
@@ -63,9 +178,5 @@ export default {
 #signup {
 
 
-}
-
-.md-field::after {
-  display: none;
 }
 </style>
