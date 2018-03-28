02fadd7c986d185470f9f79eaad12902dc59adfb
Quentin Oschatz <quentin@cliffhouse.de>
2018-03-28 09:46:17 +0200

    fix: Fix app.js
    Fix port overlap in app.js

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
+
+app.use(serveStatic(__dirname + "client/dist"));
+
+var port = process.env.PORT || 3000;
+
 app.listen(port);
 console.log('server started '+ port);
